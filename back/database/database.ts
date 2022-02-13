597ef4de550384d81f37c13b2e13345c39fa37a8
Daniel Dupriest <kououken@gmail.com>
2022-02-13 14:00:35 -0800

    Finished adding types to database.ts.

--- back/database/database.ts
+++ back/database/database.ts
@@ -1,10 +1,15 @@
-require("dotenv").config()
-const sqlite3 = require("sqlite3").verbose()
-const dbFile = process.env.DATABASE_FILE
+import { config } from "dotenv";
+config();
+import sqlite3 from "sqlite3"
+sqlite3.verbose()
+
+const dbFile = process.env.DATABASE_FILE || "database.db"
 
 export default new sqlite3.Database(dbFile, (err) => {
     if (err) {
-        return console.error(err.messagae)
+        if (err instanceof Error)
+            return console.error(err.message)
+        return console.error("Something went wrong opening database.")
     }
     console.log("Connected to SQlite database")
 })
